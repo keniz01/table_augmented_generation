4d6c5abbc40a7041d312d5239572c830978b71e0
Kenneth Kiiza <keniz01@Kenneths-MacBook-Air.local>
2025-01-31 20:25:30 +0000

    Fix models folder path.

--- src/language_models.py
+++ src/language_models.py
@@ -1,10 +1,12 @@
+from os import path
 from llama_cpp import LLAMA_POOLING_TYPE_LAST, Llama
 
-class EmbeddingModel():
-    
+model_path=path.abspath(path.join(__file__ ,"../../.."))
+
+class EmbeddingModel():    
     def __init__(self):
         self.__embed_model=Llama(
-            model_path='../../models/bge-small-en-v1.5-q4_k_m.gguf',
+            model_path=f"{model_path}/models/bge-small-en-v1.5-q4_k_m.gguf",
             embedding=True,
             verbose=False,
             pooling_type=LLAMA_POOLING_TYPE_LAST
@@ -17,7 +19,7 @@ class EmbeddingModel():
 class InstructionModel():
     def __init__(self):
         self.model = Llama(
-            model_path="../../models/Phi-3.5-mini-instruct-Q6_K_L.gguf",
+            model_path=f"{model_path}//models/Phi-3.5-mini-instruct-Q6_K_L.gguf",
             n_ctx=2048,
             verbose=False,
             n_gpu_layers=0,
